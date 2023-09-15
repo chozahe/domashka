9c1c161d16b9f2ae8d762bd44dc4366b4e6dee2a
chozahe <144026241+chozahe@users.noreply.github.com>
2023-09-15 22:35:16 +0300

    Update Main3.java

--- Main3.java
+++ Main3.java
@@ -1,6 +1,6 @@
 import java.lang.Math;
 import java.util.Scanner;
-public class Main {
+public class Main3 {
     public static void main(String[] args) {
        Scanner scan = new Scanner(System.in);
        int  year, animal;
